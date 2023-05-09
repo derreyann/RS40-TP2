ea929596ce0e38ae9ef162ff0f936c2ad9687a84
Yann Derré <yann.derre44@gmail.com>
2023-05-09 18:44:58 +0200

    fini tp2

---
+++ run_server.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+
+Created on May 2022
+@author: Mr ABBAS-TURKI
+
+"""
+
+from flask import Flask, request
+import ssl
+
+# définir le message secret
+SECRET_MESSAGE = "bigjoe bigman" # A modifier
+app = Flask(__name__)
+USER1_LOGIN = "bigjoe"
+USER1_PASSWORD = "boss"
+
+
+@app.route("/", methods=['GET', 'POST'])
+
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if username == USER1_LOGIN and password == USER1_PASSWORD:
+            return ("Logged in successfully! \n" + SECRET_MESSAGE)
+        else:
+            return 'Wrong username or password!'
+    
+    return '<form action="" method="post">\n <p><input type=text name=username /></p>\n <p><input type=password name=password /></p>\n <p><input type=submit value=Login /></p>\n </form>'
+
+if __name__ == "__main__":
+    # HTTP version
+    #app.run(debug=True, host="0.0.0.0", port=8081)
+    # HTTPS
+    ssl_context = ("server-public-key.pem", "server-private-key.pem")
+    app.run(debug=True, host="0.0.0.0", port=8082, ssl_context=ssl_context)
